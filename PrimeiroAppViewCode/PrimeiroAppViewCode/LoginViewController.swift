536a46673b905e11762111ab92e64f9c13114e4c
Caio Fabrini <caiofabrini@outlook.com>
2024-07-17 22:59:22 -0300

    criando e configurando projeto para a utilizacao do viewCode

---
+++ PrimeiroAppViewCode/PrimeiroAppViewCode/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppViewCode
+//
+//  Created by Caio Fabrini on 17/07/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
