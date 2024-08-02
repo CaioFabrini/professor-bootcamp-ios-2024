2f39c1b0973b5e8f80732634b2d761358b3d57e0
Caio Fabrini <caiofabrini@outlook.com>
2024-08-01 21:25:44 -0300

    criando app com collectionView simples

---
+++ CollectionViewBootcamp2024/CollectionViewBootcamp2024/Model/Detail.swift
@@ -0,0 +1,14 @@
+//
+//  Detail.swift
+//  CollectionViewBootcamp2024
+//
+//  Created by Caio Fabrini on 01/08/24.
+//
+
+import Foundation
+
+struct Detail {
+  var text: String
+  var image: String
+}
+
