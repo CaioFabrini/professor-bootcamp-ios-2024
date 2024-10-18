107dc6f47954f4422a1aed51c2170ce81d653e36
Caio Fabrini <caiofabrini@outlook.com>
2024-10-17 22:53:25 -0300

    criando primeiro projeto com swiftUI

---
+++ PrimeiroAppSwiftUIBootcamp/PrimeiroAppSwiftUIBootcamp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  PrimeiroAppSwiftUIBootcamp
+//
+//  Created by Caio Fabrini on 17/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Text("Caio Fabrini")
+      Text("Olá mundo")
+      Text("Olá mundo")
+      Text("Olá mundo")
+      Text("Olá mundo")
+      Text("Olá mundo")
+      Text("Olá mundo")
+      Text("Olá mundo")
+      HStack {
+        Text("Olá mundo")
+        Text("Olá mundo")
+        Text("Olá mundo")
+      }
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ PrimeiroAppSwiftUIBootcamp/PrimeiroAppSwiftUIBootcamp/PrimeiroAppSwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroAppSwiftUIBootcampApp.swift
+//  PrimeiroAppSwiftUIBootcamp
+//
+//  Created by Caio Fabrini on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroAppSwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
