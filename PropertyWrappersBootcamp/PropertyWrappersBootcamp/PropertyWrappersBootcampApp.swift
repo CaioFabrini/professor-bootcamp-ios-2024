00031a3ed3ba807db0831151b3f67bdc26988168
Caio Fabrini <caiofabrini@outlook.com>
2024-11-06 21:06:13 -0300

    aprendendo sobre propertyWrappers com swiftUI

---
+++ PropertyWrappersBootcamp/PropertyWrappersBootcamp/PropertyWrappersBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersBootcampApp.swift
+//  PropertyWrappersBootcamp
+//
+//  Created by Caio Fabrini on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
